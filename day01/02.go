3840ff335a936fc78778e79690f82ce6bc75b1e7
Anon Ymous <ancientstraits@protonmail.com>
2021-12-02 13:18:05 +0000

    day01

--- day01/02.go
+++ day01/02.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	// Provide this file yourself from your
 	// version of Advent of Coding.
-	file, err := os.Open("01/input")
+	file, err := os.Open("day01/input")
 	if err != nil {
 		panic(err)
 	}
@@ -18,20 +18,26 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
+	prev := 9999
+	win := [3]int{999, 999, 999}
 	ans := 0
-	prev := 99999
+
 	for scanner.Scan() {
-		curr, err := strconv.Atoi(scanner.Text())
+		win[0] = win[1]
+		win[1] = win[2]
+		i, err := strconv.Atoi(scanner.Text())
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
+		win[2] = i
 
-		if curr > prev {
+		sum := win[0] + win[1] + win[2]
+		if sum > prev {
 			ans++
 		}
 
-		prev = curr
+		prev = sum
 	}
 
 	if err := scanner.Err(); err != nil {
